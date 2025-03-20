2993a9b0aa74425792845fec0ff798f5c7949840
Sergey <Smirnovsd2405@yandex,ru>
2025-03-20 17:08:21 +0400

    commit

---
+++ main.py
@@ -0,0 +1,19 @@
+from flask import Flask, render_template
+
+app = Flask(__name__)
+
+
+@app.route('/distribution')
+def distribution():
+    astronauts = [
+        "Капитан Джон Смит",
+        "Астронавт Алан Бин",
+        "Астронавт Нил Армстронг",
+        "Астронавт Юрий Гагарин",
+        "Астронавт Валентина Терешкова"
+    ]
+    return render_template('distribution.html', astronauts=astronauts)
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
